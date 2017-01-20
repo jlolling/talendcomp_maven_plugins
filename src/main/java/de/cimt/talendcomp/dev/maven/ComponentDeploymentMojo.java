8489fd9d77eb4fdf507d558959812839919a8f12
Jan Lolling <jan.lolling@gmail.com>
2017-01-20 20:16:13 +0100

    release 1.5

--- src/main/java/de/cimt/talendcomp/dev/maven/ComponentDeploymentMojo.java
+++ src/main/java/de/cimt/talendcomp/dev/maven/ComponentDeploymentMojo.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 Jan Lolling jan.lolling@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.cimt.talendcomp.dev.maven;
 
 import java.io.File;
@@ -151,10 +166,8 @@ public class ComponentDeploymentMojo extends AbstractMojo {
         }
         getLog().info("Process component XML configuration...");
         try {
-            if (noJars == false) {
-                getLog().info("    setup imports...");
-                util.setupXMLImports();
-            }
+            getLog().info("    setup imports...");
+            util.setupXMLImports();
             if (addReleaseLabel) {
                 getLog().info("    setup release and version info...");
                 util.setupXMLReleaseLabel();
