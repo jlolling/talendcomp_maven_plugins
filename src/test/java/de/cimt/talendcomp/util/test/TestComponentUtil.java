c5f9e97d169e50607d363b674471eedc18019a44
Jan Lolling <jan.lolling@gmail.com>
2018-07-20 22:56:16 +0200

    test case reconfigured and pom enhanced to fulfill the requirements to publish it to central

--- src/test/java/de/cimt/talendcomp/util/test/TestComponentUtil.java
+++ src/test/java/de/cimt/talendcomp/util/test/TestComponentUtil.java
@@ -11,11 +11,8 @@ public class TestComponentUtil {
 	@Test
 	public void testReadAndWrite() throws Exception {
 		ComponentUtil util = new ComponentUtil();
-		util.addJarFile("/Volumes/Data/Talend/workspace_talend_comp/talendcomp_ant_maven_plugins/src/test/resource/tTest/cimt-talendcomp-excel-8.0.jar");
-		util.addJarFile("/Volumes/Data/Talend/workspace_talend_comp/talendcomp_ant_maven_plugins/src/test/resource/tTest/commons-codec-1.10.jar");
-		util.addJarFile("/Volumes/Data/Talend/workspace_talend_comp/talendcomp_ant_maven_plugins/src/test/resource/tTest/commons-collections4-4.1.jar");
-		util.addJarFile("/Volumes/Data/Talend/workspace_talend_comp/talendcomp_ant_maven_plugins/src/test/resource/tTest/curvesapi-1.04.jar");
-		util.setComponentBaseDir("/Volumes/Data/Talend/workspace_talend_comp/talendcomp_ant_maven_plugins/talend_component");
+		util.addJarFile("/Data/Talend/workspace_talend_comp/talendcomp_maven_plugins/test_libs/jlo-talendcomp-json-13.2.jar");
+		util.setComponentBaseDir("/Data/Talend/workspace_talend_comp/talendcomp_maven_plugins/talend_component/");
 		util.setComponentName("tTest");
 		util.setComponentVersion("1.0");
 		util.execute();
